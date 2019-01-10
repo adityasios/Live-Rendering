949848fd5224c5b6d67261b41dbde4d68abb14e0
Aditya Kumar <webmaazix@gmail.com>
2019-01-10 17:02:52 +0530

    Initial Commit

---
+++ Live Rendering/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Live Rendering
+//
+//  Created by Rakhi on 10/01/19.
+//  Copyright © 2019 Webmaazix. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
